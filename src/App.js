f1b8651f82b795bf2f4cab37d63a08aaf1f97e2c
bek <bekrye@gmail.com>
2023-10-31 23:22:58 +0100

    Add componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,35 +7,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Max', lastName: 'Tom'},
-      company: 'ZTM',
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(
+      () => {
+        return { monsters: users };
+      },
+      () => {
+        console.log(this.state);
+      }
+    )
+    );
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState( 
-              () => {
-                return { 
-                  name: {firstName: 'John', lastName: 'Wik'},
-               }
-              },
-              () => {
-                console.log(this.state);
-              }
-            );
-            
-          }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+            </div>
+        })}
       </div>
     );
   }
