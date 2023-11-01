121065ffe8b71e98088b09a3f9d1130cf8bc2858
bek <bekrye@gmail.com>
2023-11-01 23:59:37 +0100

    Add optimization for input field

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(
@@ -25,9 +28,29 @@ class App extends Component {
     );
   }
   render() {
+    console.log('render');
+
+    const filteredmonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input 
+        className='search-box'
+        type='search'
+        placeholder='search monsters'
+        onChange={(event) => {
+          const searchField = event.target.value.toLowerCase();
+
+          this.setState(
+            () => {
+            return { searchField };
+          }
+          );
+        }}
+        />
+        {filteredmonsters.map((monster) => {
           return <div key={monster.id}>
             <h1>{monster.name}</h1>
             </div>
